3038d50bc2b0ed4fbd4ed29100f927be6bbb648b
Rikstone <tarikbouari@gmail.com>
2022-10-04 19:18:06 +0000

    style mission component

--- src/MissionsActions/MissionAction.js
+++ src/MissionsActions/MissionAction.js
@@ -1,1 +1,35 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
+
+const initalstate = [];
+
+// API url
+const url = 'https://api.spacexdata.com/v3/missions';
+
+// create action type
+// const JOIN_MISSION = 'spacehub/missions/JOIN_MISSION';
+// const LEAVE_MISSION = 'spacehub/missions/LEAVE_MISSION';
+const GET_MISSION = 'spacehub/missions/GET_MISSION';
+
+// create actions
+export const getMission = createAsyncThunk(GET_MISSION, async () => {
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data);
+    return { mission: data };
+  } catch (err) {
+    alert(err);
+    return 'Mission data failed';
+  }
+});
+
+export const misssionReducer = (state = initalstate, action) => {
+  switch (action.type) {
+    case 'spacehub/missions/GET_MISSION/fulfilled':
+      console.log(action.type);
+      console.log(action.payload.mission)
+      return action.payload.mission;
+    default:
+      return state;
+  }
+};
--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,57 +1,48 @@
-import React from 'react';
+import React,{useEffect} from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import Table from 'react-bootstrap/Table';
+import { getMission } from '../MissionsActions/MissionAction';
 
 function Mission() {
-  return (
+  let missions = useSelector((state) => state.mission);
+  const dispatch = useDispatch();
+  useEffect(() =>{
+    dispatch(getMission());
+  }, []);
+  console.log(missions);
+  return (  
     <div>
-      <Table striped bordered hover>
-        <thead>
-          <tr>
-            <th> Mission</th>
-            <th>Description</th>
-            <th> Status</th>
-            <th> &nbsp;</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td> Thaicom</td>
-            <td>
-              Qui cillum aute anim excepteur anim duis. Consectetur magna nostrud
-              adipisicing ad aliqua aliquip ipsum sit sunt. Excepteur nulla quis ut
-              dolore ut nisi aute amet. Quis aute consequat eiusmod laboris ullamco.
-              Culpa dolor elit anim mollit nostrud nostrud ipsum dolor Lorem mollit
-              excepteur pariatur.
-              {' '}
-            </td>
-            <td>
-              <button type="button">Active Member</button>
-            </td>
-            <td>
-              <button type="button">Leave Mission</button>
-            </td>
-          </tr>
-
-          <tr>
-            <td> Thaicom</td>
-            <td>
-              Qui cillum aute anim excepteur anim duis. Consectetur magna nostrud
-              adipisicing ad aliqua aliquip ipsum sit sunt. Excepteur nulla quis ut
-              dolore ut nisi aute amet. Quis aute consequat eiusmod laboris ullamco.
-              Culpa dolor elit anim mollit nostrud nostrud ipsum dolor Lorem mollit
-              excepteur pariatur.
-              {' '}
-            </td>
-            <td>
-              <button type="button">Active Member</button>
-            </td>
-            <td>
-              <button type="button">Leave Mission</button>
-            </td>
-          </tr>
-        </tbody>
-
-      </Table>
+      <div>
+        {' '}
+      </div> 
+              <Table  bordered>
+              <thead>
+                <tr>
+                  <th> Mission</th>
+                  <th>Description</th>
+                  <th> Status</th>
+                  <th> &nbsp;</th>
+                </tr>
+              </thead>
+              { missions.map((item) => (
+              <tbody>
+                <tr key={item.mission_id}>
+                  <td> {item.mission_name}</td>
+                  <td> 
+                    {item.description}
+                  </td>
+                  <td colSpan={1}>
+                    <span type="button">Active Member</span>
+                  </td>
+                  <td>
+                    <button type="button">Leave Mission</button>
+                  </td>
+                </tr>
+              </tbody>
+          
+      )
+            )};
+          </Table>
     </div>
   );
 }
