4cf4222cf2a2b553e4c1f8b21558be2ed845b3ce
Rikstone <tarikbouari@gmail.com>
2022-10-07 05:37:55 +0000

    Implement join mission action

--- src/components/Mission.js
+++ src/components/Mission.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { joinMission,leaveMisson} from '../redux/profile/profile';
+import { useDispatch, useSelector } from 'react-redux';
+
 
 const Mission = (props) => {
   const { missionName, missionDesc } = props;
+  const joinedMission = useSelector((state) => state.profile.joined);
+  const dispatch = useDispatch();
+
+  const handleJoin = (e) =>{
+    e.preventDefault();
+    const name = missionName;
+    if(joinedMission.includes(name)){ 
+      return
+    }
+    dispatch(joinMission(name))
+  }
+  const handleLeave = (e) =>{
+    e.preventDefault();
+    dispatch(leaveMisson(missionName))
+  }
 
   return (
     <tr>
@@ -11,13 +29,29 @@ const Mission = (props) => {
         {missionName}
       </td>
       <td>{missionDesc}</td>
+      {joinedMission.includes(missionName) && (
       <td colSpan={1}>
         <span className="badge-primary">Active Member</span>
       </td>
-      <td>
-        <button type="button">Join Mission</button>
+      )}
+
+      {!joinedMission.includes(missionName) && (
+      <td colSpan={1}>
+        <span className="badge-primary">Not a Member</span>
       </td>
+      )}
+      {joinedMission.includes(missionName) && (
+      <td>
+        <button type="button" onClick= {handleLeave}>Join Mission</button>
+      </td>
+      )}
+      {!joinedMission.includes(missionName) && (
+      <td>
+        <button type="button" onClick= {handleJoin}>Join Mission</button>
+      </td>
+      )}
     </tr>
+
 
   );
 };
